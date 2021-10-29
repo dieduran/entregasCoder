787d1c6c93d184979e9b7c2430f540c7f0df032d
Diego Duran <diego.duran@gmail.com>
2021-10-28 23:31:35 -0300

    agregamos entrega de clase05 actualizada

---
+++ Clase05/hbs/main.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const routerProductos= require('./router/routerProducto')
+const handlebars= require('express-handlebars')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.engine('hbs', handlebars({
+    extname: 'hbs',
+    defaultLayout: 'default',
+    layoutDir: "/views/layouts",
+  }))
+ 
+
+app.set('view engine', 'hbs');
+app.set('views', "./views");
+
+/** -------------  */
+/*  cargo routers*/
+
+app.use('/productos',routerProductos)
+
+/** -------------  */
+const PORT=8080
+const server = app.listen(PORT, () => {
+    console.log(`Conectado al puerto ${server.address().port}`)
+})
+server.on('error', (error) => {
+    console.log('Ocurrio un  error...')
+    console.log(error)})
